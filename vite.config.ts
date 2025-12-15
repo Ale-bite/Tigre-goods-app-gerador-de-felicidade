605f5e6cf19cf06fb4409d3effedf341143fec43
Ale-bite <alessandro.nqbenites@gmail.com>
2025-12-15 08:10:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
+const ambiente = process.env
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
